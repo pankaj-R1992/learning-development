31cf5a0e682eb7ea0c562797657efb1cadadbf4a
pankajdx48 <dixitpankaj48@gmail.com>
2019-11-10 01:36:34 +0530

    Added first comment to start new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="vertical-align" >Hello Madam Ji</span>
+    </div>
+  );
+}
+
+export default App;
